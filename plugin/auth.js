8c4bee32c0c6a8630df54a38f65b05a72a372983
Paulhakeem <paulnyamawi18@gmail.com>
2024-01-02 23:27:33 +0300

    adding firebase authentication and firestore

---
+++ plugin/auth.js
@@ -0,0 +1,10 @@
+export default defineNuxtPlugin(() => {
+    addRouteMiddleware('auth', () => {
+        const {$auth} = useNuxtApp()
+
+        console.log($auth);
+        if(!$auth?.currentUser?.uid) {
+            return navigateTo('/signUp')
+        }
+    })
+})
