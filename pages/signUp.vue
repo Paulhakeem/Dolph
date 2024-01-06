2f0a93924fbdb556b464a09016c10bc684d5fea3
Paulhakeem <paulnyamawi18@gmail.com>
2024-01-06 18:18:57 +0300

    make some changes

--- pages/signUp.vue
+++ pages/signUp.vue
@@ -26,6 +26,7 @@
 
             <div class="mt-5">
               <button
+              @click="google"
                 type="button"
                 class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
               >
@@ -181,6 +182,7 @@ const provider = new GoogleAuthProvider();
         user.value = userCredentials.user;
         useNuxtApp().$toast.success("Welcome back!");
       }
+       await navigateTo({ path: "/booking" });
     } catch (error) {
       useNuxtApp().$toast.error("An error occurs!!");
     }
