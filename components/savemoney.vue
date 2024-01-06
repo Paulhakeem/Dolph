2c6587f91ccf2ce38986bbb0b55e3b9e064177de
Paulhakeem <paulnyamawi18@gmail.com>
2024-01-06 16:09:15 +0300

    Done with responsiveness

--- components/savemoney.vue
+++ components/savemoney.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
-    <div class="bg-bg w-full h-auto">
-    <div class="flex flex-wrap justify-center gap-20 pb-32">
-      <div id="imageposition" class="relative w-96">
-        <div class="absolute inset-x-0 top-0">
-          <img
+    <div class="bg-bg w-full h-auto mx-auto">
+      <div
+        class="grid grid-cols-1 md:grid-cols-2 pb-12 items-center justify-center gap-5"
+      >
+        <div class="justify-center justify-self-center mx-auto">
+          <div class="flex justify-center object-center">
+            <img
             src="../assets/images/lambo.jpg"
             alt=""
-            class="origin-top-left"
+            class="w-96"
           />
-          <div id="button" class="flex justify-center gap-4 px-2">
-            <div id="button1"
+          </div>
+          <div id="button" class="flex text-center justify-center gap-4 px-2">
+            <button
               class="flex justify-center gap-4 border border-1 border-light p-3 rounded-md w-52"
             >
               <p class="text-4xl font-bold text-light">+200</p>
@@ -18,9 +21,9 @@
                 Cars in <br />
                 our fleet
               </p>
-            </div>
+            </button>
 
-            <div id="button2"
+            <button
               class="flex justify-center gap-4 border border-1 border-light p-3 rounded-md w-52"
             >
               <p class="text-4xl font-bold text-light">50k</p>
@@ -28,32 +31,36 @@
                 Satisified <br />
                 customers
               </p>
-            </div>
+            </button>
           </div>
         </div>
-      </div>
 
-      <div id="money" class="pt-16 tablet:pt-74 lg:pt-14">
-        <h4 class="text-light font-black text-4xl text-pretty">
-          Save money <br />
-          with our rental
-        </h4>
-        <p class="text-light pt-4 text-sm text-pretty">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
-          Reprehenderit, itaque?
-        </p>
-        <div id="moneybutton">
-          <button 
-          class="bg-light-blue rounded-full p-2 w-44 mt-10 text-md font-medium text-heading"
+        <div
+          id="saveMoney"
+          class="pt-6 justify-center justify-self-center mx-auto"
         >
-          Booking Now
-        </button>
+          <h4
+            id="savemoneyH"
+            class="text-light font-black text-4xl text-pretty"
+          >
+            Save money <br />
+            with our rental
+          </h4>
+          <p class="text-light pt-4 text-sm text-pretty">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+            Reprehenderit, itaque?
+          </p>
+
+          <button
+            class="bg-light-blue rounded-md p-2 w-42 mt-10 text-md font-medium text-heading text-sm"
+          >
+            Booking Now
+          </button>
         </div>
       </div>
     </div>
-  </div>
 
-  <popular/>
+    <popular />
   </main>
 </template>
 
